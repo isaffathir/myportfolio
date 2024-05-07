83d492b351f093298aad2294889e2e44b0a6f1bc
isaffathir <isaffathir@outlook.com>
2024-05-07 09:57:56 +0700

    add virtual desktop windows 11

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,38 @@
+"use client";
 import React from "react";
+import { LampContainer } from "@/components/ui/lamp";
+import { motion } from "framer-motion";
 
 const AboutPage = () => {
-  return <div>About</div>;
+  return (
+    <div className="flex-row w-[90vw] md:w-[60vw] lg:w-[50vw] xl:w-[60vw] rounded-xl">
+      <div className="flex-row w-full h-full">
+        <motion.h3 className="z-50 bg-gradient-to-br from-slate-300 to-slate-500 bg-clip-text text-center text-5xl font-medium tracking-tight text-transparent md:text-7xl">
+          Explore My
+          <br /> Virtual Desktop
+        </motion.h3>
+        <LampContainer className="min-h-[130vh]">
+          <motion.h1
+            initial={{ opacity: 0.5, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.3,
+              duration: 0.8,
+              ease: "easeInOut",
+            }}
+            className="mt-0 bg-gradient-to-br from-slate-300 to-slate-500 py-0 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+          >
+            <div className="flex items-center justify-center bg-gradient-to-br from-cyan-400 to-cyan-900 h-[60vh] w-[90vw] md:w-[60vw] lg:w-[50vw] xl:w-[45vw] rounded-xl tracking-tight">
+              <iframe
+                src="https://wintest.andrewstech.me/"
+                className="m-1 h-[59vh] w-[90vw] md:w-[60vw] lg:w-[50vw] xl:w-[45vw] rounded-xl tracking-tight"
+              ></iframe>
+            </div>
+          </motion.h1>
+        </LampContainer>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,15 +20,15 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="w-full h-full bg-[#1e1e2e]">
-          <div className="py-[7rem]">
-            <div className="fixed top-0 w-full h-15 md:h-24 lg:h-24 xl:h-24 justify-center items-center bg-[#1e1e2e]/90 backdrop-filter backdrop-blur-sm">
-              <Navbar />
-            </div>
-            <div className="md:hidden lg:hidden xl:hidden fixed bottom-0 w-full h-24 justify-center items-center bg-[#1e1e2e]/90 backdrop-filter backdrop-blur-sm">
-              <BottomNavbar />
-            </div>
-            <div className="flex justify-center h-full">{children}</div>
+          {/* <div className="my-[6rem]"> */}
+          <div className="fixed z-50 top-0 w-full h-15 md:h-24 lg:h-24 xl:h-24 justify-center items-center bg-[#1e1e2e]/90 backdrop-filter backdrop-blur-sm">
+            <Navbar />
           </div>
+          <div className="md:hidden z-50 lg:hidden xl:hidden fixed bottom-0 w-full h-24 justify-center items-center bg-[#1e1e2e]/90 backdrop-filter backdrop-blur-sm">
+            <BottomNavbar />
+          </div>
+          <div className="flex justify-center my-24 ">{children}</div>
+          {/* </div> */}
         </div>
       </body>
     </html>
